5594c1474ba980e768f63b34081bf77d8f7b9ddc
aikkyy <carolina.pestana.aguiar@gmail.com>
2023-12-15 21:31:18 +0000

    Capture the user's name from the input and store it in local storage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
-const renderPage = async (pageName: string): Promise<void> => {
-  const html = await fetch(`pages/${pageName}.html`).then((resp) =>
-    resp.text()
-  );
+// #1 Fetch HTML page and render in root element
+const renderPage = (pageName: string) => {
+  const html = fetch(`pages/${pageName}.html`)
+    .then((resp) => resp.text())
+    .then((html) => {
+      //get root
+      const rootElem = document.getElementById("root") as HTMLElement;
+      rootElem.innerHTML = html;
 
-  const rootElem = document.getElementById("root") as HTMLElement;
-  rootElem.innerHTML = html;
+      // #2 Capture the user's name from the input and store it in local storage
+      // get form
+      const formElem = document.getElementById("form-start") as HTMLFormElement;
+
+      // function to handle submit
+      const handleSubmit = (event: Event): void => {
+        event.preventDefault();
+        // get user input
+        const userInput = document.getElementById(
+          "input-name"
+        ) as HTMLInputElement;
+
+        let userName: string = userInput.value;
+
+        // check if input value is empty
+        if (userName !== "") {
+          localStorage.setItem("name", userName);
+          // clear the input field
+          userInput.value = "";
+        } else {
+          alert("Please enter your name.");
+        }
+      };
+
+      formElem.addEventListener("submit", handleSubmit);
+    });
 };
+
+renderPage("start");
